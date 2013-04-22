65da51e44926c1776495382c1f5ead9e2142a32d
Jack Jamieson <jamieson.jack@gmail.com>
2013-04-22 18:42:53 -0400

    Longest words and search function added.

---
+++ src/php/longest.php
@@ -0,0 +1,49 @@
+<!--
+Written by Jack Jamieson
+https://twitter.com/jamieson_jack
+
+Reddit Front Page Word Analyzer - posts.php
+Displays the top 25 words from the raw text file.
+
+//    This file is part of RedditFPWAnalyzer.
+//
+//    RedditFPWAnalyzer is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    RedditFPWAnalyzer is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with RedditFPWAnalyzer.  If not, see <http://www.gnu.org/licenses/>.
+-->
+<?php
+$text = file_get_contents('http://www.tcnj.edu/~jamiesj1/rwa/ALLFrontPagedWords.txt');
+
+$array = preg_split("/\r\n|\n|\r/", $text);#split by new line and put into an array.
+
+$longarr = str_replace("Seen", "", $array);//All longarr manipulations are to get just the word. Strip away all other parts.
+
+$longarr = array_filter(array_map('trim', $longarr));
+$longarr = preg_replace('/\d/', '', $longarr);
+$longarr = array_filter(array_map('trim', $longarr));
+$longarr = preg_replace("/[^a-zA-Z 0-9 \- \/ \( \)]+/", "", $longarr);
+
+function sortbyL($a,$b){	//Custom sort by string length.  Some problems with UTF-8 encoding, but seems to work for my purposes.
+    return strlen($b) - strlen($a);
+}
+
+usort($longarr,'sortbyL');
+
+$newarr = array_slice($longarr, 0, 25);#top 25.
+$comma = implode("\n", $newarr);#seperate the array back into new lines.
+
+echo '<pre>'; print_r($comma); echo '</pre>'
+?>
+
+
+
+
---
+++ src/php/process.php
@@ -0,0 +1,65 @@
+<!--
+Written by Jack Jamieson
+https://twitter.com/jamieson_jack
+
+Reddit Front Page Word Analyzer - posts.php
+Displays the top 25 words from the raw text file.
+
+//    This file is part of RedditFPWAnalyzer.
+//
+//    RedditFPWAnalyzer is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    RedditFPWAnalyzer is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with RedditFPWAnalyzer.  If not, see <http://www.gnu.org/licenses/>.
+-->
+<?php
+$text = file_get_contents('http://www.tcnj.edu/~jamiesj1/rwa/ALLFrontPagedWords.txt');
+$array = preg_split("/\r\n|\n|\r/", $text);
+
+$sentData = $_POST['title'];#Data from the index page.
+
+$searchthis = $sentData;
+$matches = array();
+$ignore = "Seen";
+
+$handle = @fopen("http://www.tcnj.edu/~jamiesj1/rwa/ALLFrontPagedWords.txt", "r");#Open the file, read-only.
+if ($handle)
+{
+    while (!feof($handle))
+    {
+        $buffer = fgets($handle);
+        if(strpos($buffer, $searchthis) !== FALSE)
+			if($searchthis == $ignore)#Don't allow users to type Seen...it's part of all strings.
+			{
+				echo("Invalid word.");
+				break;
+			}
+			else{
+				$matches[] = $buffer;
+				}
+    }
+    fclose($handle);
+}
+natsort($matches);#Show the highest at the top.
+$matches = array_reverse($matches, false);#descending order.
+if(empty($matches))
+{
+	echo '<pre>'."No matches found.".'</pre>';
+}
+
+$newarr = array_slice($matches, 0, 5);#show only 5 results.
+$comma = implode("\n", $newarr);#seperate the array back into new lines.
+
+echo '<pre>'; print_r($comma); echo '</pre>'
+?>
+
+
+
